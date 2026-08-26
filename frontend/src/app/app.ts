4b5a087389a83c95d0a3227792b0a6e5d932c490
GaelDD2 <gaelortega2607@gmail.com>
2026-08-26 07:21:05 -0600

    Se corrige error de no mantener la sesion al recargar la pagina

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,20 +1,24 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { toast, NgxSonnerToaster } from 'ngx-sonner';
+import { NgxSonnerToaster } from 'ngx-sonner';
 import { AuthService } from './core/services/auth.service';
-
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NgxSonnerToaster],
+  standalone: true,
+  imports: [RouterOutlet, NgxSonnerToaster,MatProgressSpinnerModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('app');
+export class App implements OnInit {
   private readonly authService = inject(AuthService);
 
+  sesionLista = signal(false);
+
   ngOnInit(): void {
-    this.authService.inicializarSesion().subscribe();
+    this.authService.inicializarSesion().subscribe({
+      complete: () => this.sesionLista.set(true),
+      error:    () => this.sesionLista.set(true),
+    });
   }
 }
-
